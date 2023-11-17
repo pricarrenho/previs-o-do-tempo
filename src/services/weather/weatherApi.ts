4e6104f228ed517359775000c2e969ba69990c3c
Priscilla <priscillaonil@gmail.com>
2023-11-17 17:49:31 -0300

    fix: https bug

--- src/services/weather/weatherApi.ts
+++ src/services/weather/weatherApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const weatherApi = axios.create({
-  baseURL: "http://api.weatherapi.com/v1/forecast.json",
+  baseURL: "https://api.weatherapi.com/v1/forecast.json",
   params: {
     key: import.meta.env.VITE_WEATHER_API_KEY,
     lang: "pt",
